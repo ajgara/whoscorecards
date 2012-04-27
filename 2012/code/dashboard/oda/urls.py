4a82795febb3b9fb8613ad0af4823ba5da96f9c0
Adi Eyal <adi@burgercom.co.za>
2012-04-28 00:30:13 +0200

    Added data view

--- 2012/code/dashboard/oda/urls.py
+++ 2012/code/dashboard/oda/urls.py
@@ -4,4 +4,5 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
     (r'^scorecard/front/$', direct_to_template, {"template" : "oda/scorecard_front.html"}, "scorecard_front"),
     (r'^scorecard/back/$', direct_to_template, {"template" : "oda/scorecard_back.html"}, "scorecard_back"),
+    (r'^data/(?P<iso3>\w+)/$', 'oda.views.country_data', {}, "country_data"),
 )
--- 2012/code/dashboard/oda/views.py
+++ 2012/code/dashboard/oda/views.py
@@ -2,4 +2,20 @@ from django.views.generic.simple import direct_to_template
 from django.template.loader import render_to_string
 from django.template import loader, Context
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+import json
+from collections import defaultdict
+
+import models
+
+def country_data(request, iso3):
+    country = get_object_or_404(models.Recipient, iso3=iso3) 
+    indicators = models.CountryIndicator.objects.filter(country=country)
+    years = defaultdict(dict, {})
+    for indicator in indicators:
+        year = years[indicator.year]
+        year[indicator.indicator.id] = indicator.value
+    
+    return HttpResponse(json.dumps(years))
+     
 
