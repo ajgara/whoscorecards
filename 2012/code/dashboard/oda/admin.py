aa9752d242b9901b9dd0a34b313930b34c3f430a
Adi Eyal <adi@burgercom.co.za>
2012-04-27 23:24:47 +0200

    Added general indicator models

--- 2012/code/dashboard/oda/admin.py
+++ 2012/code/dashboard/oda/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 import models
 
 admin.site.register(models.Recipient)
+admin.site.register(models.GeneralIndicator)
+admin.site.register(models.CountryIndicator)
--- 2012/code/dashboard/oda/models.py
+++ 2012/code/dashboard/oda/models.py
@@ -12,3 +12,15 @@ class Recipient(Country):
     
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.iso3)
+
+class GeneralIndicator(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
+class CountryIndicator(models.Model):
+    country = models.ForeignKey(Recipient)
+    indicator = models.ForeignKey(GeneralIndicator)
+    year = models.CharField(max_length=4)
+    value = models.FloatField()
