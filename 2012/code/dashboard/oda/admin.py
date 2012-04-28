9d093dd79ff5d21081934758bbb87d613e597d88
Adi Eyal <adi@burgercom.co.za>
2012-04-28 22:16:18 +0200

    Added admin models for allocation

--- 2012/code/dashboard/oda/admin.py
+++ 2012/code/dashboard/oda/admin.py
@@ -5,6 +5,12 @@ class CountryIndicatorAdmin(admin.ModelAdmin):
     list_filter = ("country", "indicator", "year")
     list_display = ("country", "indicator", "year", "value")
 
+class AllocationAdmin(admin.ModelAdmin):
+    list_filter = ("country", "mdgpurpose", "year")
+    list_display = ("country", "mdgpurpose", "year", "commitment", "disbursement")
+
 admin.site.register(models.Recipient)
 admin.site.register(models.GeneralIndicator)
 admin.site.register(models.CountryIndicator, CountryIndicatorAdmin)
+admin.site.register(models.MDGPurpose)
+admin.site.register(models.Allocation, AllocationAdmin)
