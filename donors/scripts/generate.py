e2da78b674d2c5f2fe42663a7b47ab018b81acb7
Adi Eyal <adi@burgercom.co.za>
2013-06-12 12:58:21 +0200

    General generate script improvement
    Refactored a tiny bit - more could be done
    Fixed bug where files with weird characters, e.g. brackets are not generated

--- donors/scripts/generate.py
+++ donors/scripts/generate.py
@@ -5,39 +5,45 @@ from time import sleep
 from pyPdf import PdfFileWriter, PdfFileReader
 
 def donors(path='./data/'):
-    donors = []
+    donors = set()
     for f in os.listdir(path):
         if f.endswith('.csv'):
             fd = open(path + f, 'r')
             _ = fd.readline()
             for line in fd.readlines():
                 donor = line.split(',')[0]
-                if donor not in donors and donor != 'totals':
-                    donors.append(donor)
-    return donors
+                if donor != 'Grand Total':
+                    donor = donor.replace('"', "")
+                    donors.add(donor)
+    return list(donors)
     
+
+page1_svg = "svg/who_donor_page1.svg"
+page2_svg = "svg/who_donor_page2.svg"
 
 def generate(donor):
     os.system('mkdir -p output')
     donor_url = donor.replace(' ','%20')
     page1 = 'output/%s1' % (donor.replace(' ','-').lower())
     page2 = 'output/%s2' % (donor.replace(' ','-').lower())
-    combined = 'output/%s' % (donor.replace(' ','-').lower())
-    os.system('cp svg/who_donor_page1.svg %s.svg' % (page1))
-    os.system('sed "s|/France/|/%s/|" svg/who_donor_page1.svg > %s.svg' % (donor_url, page1))
-    os.system('inkscape  --file=%s.svg --verb=za.co.widgetlabs.update --verb=FileSave --verb=FileQuit' % (page1))
-    os.system('inkscape --file=%s.svg --export-pdf=%s.pdf' % (page1, page1))
-    os.system('cp svg/who_donor_page2.svg %s.svg' % (page2))
-    os.system('sed "s|/France/|/%s/|" svg/who_donor_page2.svg > %s.svg' % (donor_url, page2))
-    os.system('inkscape --file=%s.svg --verb=za.co.widgetlabs.update --verb=FileSave --verb=FileQuit' % (page2))
-    os.system('inkscape --file=%s.svg --export-pdf=%s.pdf' % (page2, page2))
+
+    combined = 'output/%s.pdf' % (donor.replace(' ','-').lower())
+    if os.path.exists(combined): return
+
+    os.system('cp "%s" "%s.svg"' % (page1_svg, page1))
+    os.system('sed "s|/France/|/%s/|" "%s" > "%s.svg"' % (donor_url, page1_svg, page1))
+    os.system('inkscape  --file="%s.svg" --verb=za.co.widgetlabs.update --verb=FileSave --verb=FileQuit 2> /dev/null' % (page1))
+    os.system('inkscape --file="%s.svg" --export-pdf="%s.pdf" 2> /dev/null' % (page1, page1))
+    os.system('cp "%s" "%s.svg"' % (page2_svg, page2))
+    os.system('sed "s|/France/|/%s/|" "%s" > "%s.svg"' % (donor_url, page2_svg, page2))
+    os.system('inkscape --file="%s.svg" --export-pdf="%s.pdf" ' % (page2, page2))
     # Merge pages
     input1 = PdfFileReader(file('%s.pdf' % (page1), 'rb'))
     input2 = PdfFileReader(file('%s.pdf' % (page2), 'rb'))
     output = PdfFileWriter()
     output.addPage(input1.getPage(0))
     output.addPage(input2.getPage(0))
-    outputStream = file('%s.pdf' % (combined), 'wb')
+    outputStream = file(combined, 'wb')
     output.write(outputStream)
     outputStream.close()
     sleep(2)
@@ -45,6 +51,7 @@ def generate(donor):
 if __name__=="__main__":
     if len(sys.argv) == 1:
         for donor in donors():
+            print donor
             generate(donor)
     else:
         for donor in sys.argv[1:]:
