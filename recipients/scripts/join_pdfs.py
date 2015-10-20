756ad6f3b33a2046818ca06a457061e4ef2c4d4b
ajgara <ajgarassino@gmail.com>
2015-10-20 10:54:30 -0300

    Updated Table4_Disb_Sources. Fix in join_pdf for supporting country names with special characters on individual pdf files.

--- recipients/scripts/join_pdfs.py
+++ recipients/scripts/join_pdfs.py
@@ -2,7 +2,9 @@
 import os
 
 file_names = sorted(os.listdir("./output/"))
-pdf_file_names_arg = " ".join(map(lambda x: "./output/" + x, file_names))
+# pdf_file_names_arg = " ".join(map(lambda x: "./output/" + x, file_names))
+pdf_file_names_arg = " ".join(map(lambda x: "./output/" + "\"" + x + "\"", file_names))
+# pdf_file_names_arg = " ".join(map(lambda x: "./output/" + str.replace(str.replace(str.replace(x,"(","\("),")","\)"),"'","\'"), file_names))
 
 print("Joining %s " % pdf_file_names_arg)
 os.system("pdfjoin %s --outfile all_countries.pdf" % pdf_file_names_arg)
